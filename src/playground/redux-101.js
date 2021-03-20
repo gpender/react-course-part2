a78ba3f45d010aad0a594721429b9ed3a0522192
gpender <guy.pender@me.com>
2021-03-20 17:25:09 +0000

    Added some REDUX ACTIONS

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,40 @@
 import{createStore} from 'redux';
 
-const store = createStore((state = {count:0})=>{
-    return state;
+const store = createStore((state = {count:0}, action)=>{
+    console.log('running');
+    switch(action.type){
+        case 'INCREMENT':
+            return{
+                count:state.count+1
+            };
+        case 'DECREMENT':
+            return{
+                count:state.count-1
+            };
+        case 'RESET':
+            return{
+                count:0
+            };
+        default:
+            return state;
+    };
 });
 
+store.dispatch(
+{
+    type:'DECREMENT'
+});
+store.dispatch(
+{
+    type:'DECREMENT'
+});
+store.dispatch(
+{
+    type:'DECREMENT'
+});
+
+store.dispatch({
+    type:'RESET'
+})
+ 
 console.log(store.getState());
