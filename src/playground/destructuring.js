1ec28d61cbcf242870bddabe695e270a0552eff9
gpender <guy.pender@me.com>
2021-03-20 18:20:04 +0000

    Added array destructuring example

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -18,14 +18,26 @@
 //     console.log(`Its ${temperature} in ${city}, ${county}`);
 // }
 
-const book = {
-    title:'Ego is the Enemy',
-    author:'Ryan Holiday',
-    publisher:{
-        name:'Penguin'
-    }
-}
+// const book = {
+//     title:'Ego is the Enemy',
+//     author:'Ryan Holiday',
+//     publisher:{
+//         name:'Penguin'
+//     }
+// }
 
-const {name:publisherName='Self Published'} = book.publisher;
+// const {name:publisherName='Self Published'} = book.publisher;
 
-console.log(`${publisherName}`);
+// console.log(`${publisherName}`);
+
+// Array destructuring
+// const address = ['guy','99 King Drive', 'Angmering', 'East Sussex', 'BN164GB'];
+
+// const [name,street,city,county] = address;
+
+// console.log(`${name} ${street} ${city} ${county}`);
+
+const item = ['Coffe (hot)', '$2.00', '$2.50'];
+
+const [coffee,sm_price,med_price,lg_price='$5.00'] = item;
+console.log(`A large ${coffee} costs ${lg_price}`);
