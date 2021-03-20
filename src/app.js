c95c5b9c6c60073c9b0c9e98bbebadd266445c7a
gpender <guy.pender@me.com>
2021-03-20 13:30:11 +0000

    Added ReactRouter Link elements

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import { BrowserRouter, Link, Route,Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -17,10 +17,23 @@ const HelpPage = () => (
     <p>Help</p>
 );
 const NotFoundPage = () => (
-    <p>404!</p>
+    <div>
+    404! <Link to="/edit">Go Home</Link>
+    </div>
 );
+const Header=()=>(
+    <header>
+        <h1>Expensify</h1>
+        <Link to="/">Home</Link>
+        <Link to="/create">Create</Link>
+        <Link to="/edit">Edit</Link>
+        <Link to="/help">Help</Link>
+    </header>
+)
 const routes = (
     <BrowserRouter>
+        <div>
+        <Header></Header>
         <Switch>
             <Route path="/" component={ExpenseDashboardPage} exact={true} />
             <Route path="/create" component={AddExpensePage} />
@@ -28,6 +41,7 @@ const routes = (
             <Route path="/help" component={HelpPage} />
             <Route component={NotFoundPage} />
         </Switch>
+        </div>
     </BrowserRouter>
 );
 
