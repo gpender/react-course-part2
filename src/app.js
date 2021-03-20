685113ce4cbd5777e4d7f81f65c54e3854b9d866
gpender <guy.pender@me.com>
2021-03-20 13:13:01 +0000

    Added React Switch

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -16,15 +16,18 @@ const EditExpensePage= () => (
 const HelpPage = () => (
     <p>Help</p>
 );
-
+const NotFoundPage = () => (
+    <p>404!</p>
+);
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={ExpenseDashboardPage} exact={true} />
             <Route path="/create" component={AddExpensePage} />
             <Route path="/edit" component={EditExpensePage} />
             <Route path="/help" component={HelpPage} />
-        </div>
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
 );
 
