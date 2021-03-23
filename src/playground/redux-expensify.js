1e2cf3aba08d89ea7ba7b3b13cd8e8c8c2a943fe
gpender <guy.pender@me.com>
2021-03-23 17:54:19 +0000

    Added CombineReducer code

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,41 @@
 import { createStore, combineReducers} from 'redux';
 
+// ADD EXPENSE
+// REMOVE
+// EDIT
+// SET FILTER
+
+// Expesnses Reducer
+const expensesReducerDefaultState = [];
+const expensesReducer = (state = expensesReducerDefaultState, action) =>{
+    switch(action.type){
+        default:
+            return state;
+    }
+};
+const filtersDefaultState = {
+        text:'',
+        sortBy:'date',
+        startDate:undefined,
+        endDate:undefined
+};
+
+const filtersReducer = (state=filtersDefaultState, action) =>{
+    switch(action.type){
+        default:
+            return state;
+    }
+};
+
+const store = createStore(
+    combineReducers({
+        expenses:expensesReducer,
+        filters:filtersReducer
+    })
+);
+
+console.log(store.getState());
+
 const demoState = {
     expenses:[{
         id:'fegregegf',
